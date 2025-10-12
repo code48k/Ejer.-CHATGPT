915a0af1451dd89bfa9b6acf401f3787afb354f8
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-12 12:48:36 +0200

    Mostrar todas las tablas de multiplicar del 1 al 10.

---
+++ tablasMultiplicar.js
@@ -0,0 +1,15 @@
+// Mostrar todas las tablas de multiplicar del 1 al 10
+
+let numeroDinamico = 0;
+
+for (let i=1; i<=10; i++){
+    
+    for (let numeroDinamico=0; numeroDinamico<=10; numeroDinamico++){
+
+    resultadoTotal = i * numeroDinamico;
+
+    console.log(`${i} X ${numeroDinamico} = ${resultadoTotal}`);
+
+    }
+
+}
