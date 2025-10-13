989ffdb74554d043321197adf22c7d823f349d9d
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-13 19:54:48 +0200

    Recorre una cadena de texto e imprime cada letra por separado.

---
+++ recorreCadenaTexto.js
@@ -0,0 +1,13 @@
+// Recorre una cadena de texto e imprime cada letra por separado
+
+let saludo = "Hola Mundo, soy JavasCript!!!";
+
+let contenedor = "";
+
+for (let i=0; i<saludo.length; i++){
+
+    contenedor = saludo[i];
+
+    console.log(contenedor);
+    
+}
