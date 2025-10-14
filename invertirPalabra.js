4abdd0ced95c6d018ebb1a9f5e78e581b2ddcd6c
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-14 07:54:19 +0200

    Invertir una palabra en un string.

---
+++ invertirPalabra.js
@@ -0,0 +1,13 @@
+// Invertir una palabra en un string
+
+let cadena = "Voy aprendiendo a JavasCript!!!";
+
+let contedor = "";
+
+for (let i = cadena.length-1; i>=0; i--){
+
+    contedor = cadena[i];
+
+        console.log(contedor);
+
+}
