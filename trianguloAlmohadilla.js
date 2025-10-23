6404736b120128817b3c99d62488ec2e36d6405d
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-23 22:15:19 +0200

    Pirámide invertida con #.

---
+++ trianguloAlmohadilla.js
@@ -0,0 +1,17 @@
+// Pirámide invertida con #
+
+let veces = 25;
+
+for (let i=veces; i>=1; i--){
+
+    let asteriscos = "";
+
+    for (let x=0; x<i; x++){
+
+        asteriscos += "#";
+
+    }
+
+    console.log(asteriscos);
+    
+}
