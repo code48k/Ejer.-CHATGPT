f6ea6c60d321c4bdac90bdc5e975cc911efc5a3c
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-24 20:56:25 +0200

    Quiero un programa que me analice el array dado y me diga si es divisible entre 3 cada uno de sus elementos.

--- usoModulo.js
+++ usoModulo.js
@@ -1,6 +1,5 @@
 // Quiero un programa que me analice el array dado y me diga si es divisible entre 3 cada uno de sus elementos
 
-
 numArr = [8, 11, 90, 233, 17546, 225, 24, 12];
 
 for (let x=0; x<numArr.length; x++){
