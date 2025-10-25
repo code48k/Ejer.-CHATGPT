3d06f835d9128683338bb41196607aef339034e1
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-25 20:23:59 +0200

    Array: imprimir todos los elementos.

--- imprimirElementos.js
+++ imprimirElementos.js
@@ -1,6 +1,6 @@
 // Array: imprimir todos los elementos
 
-let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "JuanMa", 'Mario', true];
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "JuanMa", 'Mario', true, false];
 
 for (let i=0; i<myArr.length; i++){
 
