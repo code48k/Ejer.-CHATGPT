532da2f8dec2e07ca028f0f47b7c5b2f05561a05
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-25 13:39:23 +0200

    Crea un bucle que sume solo los números impares del 1 al 50.

--- sumarImpar.js
+++ sumarImpar.js
@@ -1,10 +1,14 @@
 // Crea un bucle que sume solo los números impares del 1 al 50
 
+let result = 0;
+
 for (let i=1; i<=50; i++){
 
     if (i % 2 != 0){
 
-        console.log(i);
+        result += i;
+
+        console.log(result);
         
     }
 }
