b349cef1db0b90608524652ad4d54b22e6668eac
*** Lucas <jmlucasdelamo@gmail.com>
2025-10-31 05:03:10 +0100

    Números primos entre 1 y 50.

--- numerosPrimos.js
+++ numerosPrimos.js
@@ -1,12 +1,12 @@
 // Números primos entre 1 y 50
 
 for (let i=2; i<=50; i++){
+    
+    let esPrimo = true;
 
-let esPrimo = true;
+    for (let x=2; x<i; x++){ 
 
-    for (let x=2; x <= i-1; x++){
-
-        if (i % x === 0){
+        if (i % x !== 0){ 
 
         esPrimo = false;
 
