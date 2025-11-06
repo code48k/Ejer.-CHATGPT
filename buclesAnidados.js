81e98f3c46220e7be9d0288eeebce3e039136f53
*** Lucas <jmlucasdelamo@gmail.com>
2025-11-07 00:25:48 +0100

    Haz dos bucles for anidados.

--- buclesAnidados.js
+++ buclesAnidados.js
@@ -1,10 +1,10 @@
 // Haz dos bucles anidados
 
-for(let e = 1; e<=15; e++){
+for (let e=1; e<=15; e++){
 
     console.log(`PRIMER FOR: PRIMERA VUELTA: ${e}`);
     
-    for(let i = 1; i<=e; i++){
+    for (let i=1; i<=e; i++){
 
     console.log(`SEGUNDO FOR: VUELTA: ${i}`);
 
