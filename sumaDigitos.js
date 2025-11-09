a35f26beb2232e40b388876a91d3ca83f6f7cfe1
*** Lucas <jmlucasdelamo@gmail.com>
2025-11-09 08:47:50 +0100

    Suma de dígitos, dado un número, calcula la suma de todos sus dígitos.

---
+++ sumaDigitos.js
@@ -0,0 +1,13 @@
+// Suma de dígitos, dado un número, calcula la suma de todos sus dígitos
+
+// Ejemplo: 1234 → 1 + 2 + 3 + 4 = 10.
+
+let resultado = 0;
+
+for (let i=1; i<=4; i++){
+
+    resultado += i;
+
+    }
+
+console.log(resultado);
