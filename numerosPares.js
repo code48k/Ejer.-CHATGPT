61fffc225b67a15adc1404c9896d27a526ade8ec
*** Lucas <jmlucasdelamo@gmail.com>
2025-11-09 15:14:23 +0100

    Dado un array, crea uno nuevo con solo los números pares.

--- numerosPares.js
+++ numerosPares.js
@@ -1,14 +1,14 @@
 // Dado un array, crea uno nuevo con solo los números pares
 
-let myArrPares = [1, 2, 4, 7, 9, 11, 22, 34, 56, 32, 3, 6];
+let myArr = [1, 2, 4, 7, 9, 11, 22, 34, 56, 32, 3, 6];
 
 let pares = [];
 
-for (let i=0; i<myArrPares.length; i++){
+for (let i=0; i<myArr.length; i++){
 
-    if (myArrPares[i] % 2 === 0){
+    if (myArr[i] % 2 === 0){
 
-      pares.push(myArrPares[i]);
+      pares.push(myArr[i]);
 
     }
 
