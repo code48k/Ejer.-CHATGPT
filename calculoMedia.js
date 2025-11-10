67acb96d99942405063a86d13272872e872d03a3
*** Lucas <jmlucasdelamo@gmail.com>
2025-11-10 02:15:46 +0100

    Calcula la media de los valores en un array de números.

---
+++ calculoMedia.js
@@ -0,0 +1,15 @@
+// Calcula la media de los valores en un array de números
+
+let myArr = [12, 21, 31, 49, 5, 16, 7, 18, 91, 10];
+
+let suma = 0;
+
+for (let i=0; i<myArr.length; i++){
+
+    suma += myArr[i];
+
+    }
+
+let media = suma / 2;
+
+console.log(media);
